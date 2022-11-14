fb0d22dfb8fbd17c6ce90827764554d3658bf5a5
Luis Herdeth <luis.herdeth@gmail.com>
2022-11-14 09:57:30 +0100

    delete params

--- src/page/App.jsx
+++ src/page/App.jsx
@@ -1,7 +1,5 @@
 import MostrarGifs from '../components/MostrarGifs';
-import { BrowserRouter, Routes, Route, useSearchParams, Navigate } from 'react-router-dom';
-
-// import { useLocation } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
 	// const query = useSearchParams();
@@ -13,7 +11,7 @@ function App() {
 			<Routes>
 				{/* <Search /> */}
 				{/* <Route path="/" element={<MostrarGifs />} /> */}
-				<Route path="/" element={<MostrarGifs  keyword="messi" />} />
+				<Route path="/" index element={<MostrarGifs keyword="messi" />} />
 				<Route path="*" element={<Navigate to="/" />} />
 			</Routes>
 		</BrowserRouter>
